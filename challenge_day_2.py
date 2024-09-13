0a7c464d90f4820953cc295923d3d1b7fa1ea79b
Nowak <vnowak@cps.k12.il.us>
2024-09-13 12:15:28 -0500

    day 2 work

---
+++ challenge_day_2.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")
+sales = float(input("How much have you made in sales this month?: "))
+amount = float(sales * .13)
+
+print(name +  " is entitled to " + str(amount) + " commissions this month!")
---
+++ testColab.py
@@ -0,0 +1,7 @@
+#this is just for testing for collab purposes
+#create an input for the user to input their age
+age = input("What is your age?: ")
+#create an input for the user to input their name
+name = input("What is you name?: ")
+#creat an input for the user to input their favortie song
+fav_song = input("What is your favorite song?: ")
